59d2443ec97094a5059d08d6df3a771261a12578
igorya7v <34253849+igorya7v@users.noreply.github.com>
2019-04-23 00:01:28 -0700

    Update PropertyRegistryContract.sol

--- contracts/PropertyRegistryContract.sol
+++ contracts/PropertyRegistryContract.sol
@@ -1,75 +1,34 @@
 pragma solidity ^0.5.0;
 
-/**
-* PropertyRegistryContract is a contract for managing records of Real Estate Properties, Owners
-* and transfers of the Ownership.
-*/
 contract PropertyRegistryContract {
 
-    /**
-    * Event for new property added logging.
-    *
-    * @param id                 The id of the new property.
-    * @param propertyAddress    The address of the new property.
-    */
     event NewPropertyAdded(uint indexed id, string indexed propertyAddress);
 
-    /**
-    * Event for property ownership set logging.
-    *
-    * @param propertyId     The id of the property.
-    * @param owner          The address of the set owner.
-    */
     event PropertyOwnershipSet(uint indexed propertyId, address indexed owner);
 
-    /**
-    * Event for property ownership transferred logging.
-    *
-    * @param propertyId     The id of the property.
-    * @param previousOwner  The previous owner of the property.
-    * @param newOwner       The new owner of the property.
-    */
     event PropertyOwnershipTransferred(
         uint indexed propertyId,
         address indexed previousOwner,
         address indexed newOwner);
 
-    /**
-    * The Property Details structure: every Property is composed of:
-    * - Property id
-    * - Property address
-    * - Property owner
-    */
+
     struct PropertyDetails {
         uint id;
         string propertyAddress;
         address owner;
     }
 
-    /**
-    * Address of the contract owner.
-    */
+
     address public contractOwner;
 
-    /**
-    * Properties Map.
-    * The key or id of the property is it's index in the properties array.
-    */
     PropertyDetails[] public properties;
 
-    /**
-    * Constructor of the Property Registry Contract.
-    * Sets the original owner of the contract to the sender account.
-    */
+
     constructor() public {
         contractOwner = msg.sender;
     }
 
-    /**
-    * Adds a new property to the registry.
-    *
-    * @param propertyAddress    The address of the property.
-    */
+
     function addProperty(string memory propertyAddress) public {
 
         // This function can ONLY be used by the owner of the contract.
@@ -88,12 +47,7 @@ contract PropertyRegistryContract {
         emit NewPropertyAdded(id, propertyAddress);
     }
 
-    /**
-    * Sets the initial property ownership.
-    *
-    * @param propertyId     The id of the property.
-    * @param owner          The address of the owner to set.
-    */
+
     function setPropertyOwnership(uint propertyId, address owner) public {
 
         // This function can ONLY be used by the owner of the contract.
@@ -109,12 +63,7 @@ contract PropertyRegistryContract {
         emit PropertyOwnershipSet(propertyId, owner);
     }
 
-    /**
-    * Allows the current property owner to transfer ownership to a new owner.
-    *
-    * @param propertyId     The id of the property to transfer.
-    * @param newOwner       The address to transfer ownership to.
-    */
+
     function transferPropertyOwnership(uint propertyId, address newOwner) public {
 
         // This function can ONLY be used by the current property owner.
